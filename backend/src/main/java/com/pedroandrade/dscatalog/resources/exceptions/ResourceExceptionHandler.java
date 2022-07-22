2e13e1230bfc41b40f1d14cac57d16963ddafc06
Pedro Andrade <andradephc11@gmail.com>
2022-07-22 18:53:17 -0300

    Find Category by id, exception handling

---
+++ backend/src/main/java/com/pedroandrade/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.pedroandrade.dscatalog.resources.exceptions;
+
+import com.pedroandrade.dscatalog.services.exceptions.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException exc, HttpServletRequest request) {
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setError("Resource not found");
+        err.setMessage(exc.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
