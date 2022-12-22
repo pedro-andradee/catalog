d1131238b058da1584f46635b1a2b309b0726b01
Pedro Andrade <andradephc11@gmail.com>
2022-12-22 17:11:03 -0300

    User CRUD

---
+++ backend/src/main/java/com/pedroandrade/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.pedroandrade.dscatalog.repositories;
+
+import com.pedroandrade.dscatalog.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
